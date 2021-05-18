f6400a16015bee17834cc0bf7efd56a0c86a5ffa
Vadym Hevlich <vege1wgw@gmail.com>
2021-05-18 20:25:23 +0300

    Fix thread_ts override in threads stream

--- tap_slack/transform.py
+++ tap_slack/transform.py
@@ -9,7 +9,6 @@ def decimal_timestamp_to_utc_timestamp(timestamp):
 def transform_json(stream, data, date_fields, channel_id=None):
 
     if data:
-        thread_ts = None
         for record in data:
             if stream == "messages":
                 # Strip out file info and just keep id
@@ -31,14 +30,16 @@ def transform_json(stream, data, date_fields, channel_id=None):
                 record.pop("parent_conversation", None)
                 record.pop("channel_id", None)
 
+            # Set the date field value for all streams
+            # Set record["thread_ts"] = record.get(date_field, None) in "messages" stream
+            # Set record["thread_ts"] to first data record date_field value in "threads" stream,
+            # so record["thread_ts"] equals to first message ts in thread
             for date_field in date_fields:
                 timestamp = record.get(date_field, None)
                 if timestamp and isinstance(timestamp, str):
-                    if stream == 'messages' or stream == "threads" and date_field == 'ts':
-                        record[date_field] = decimal_timestamp_to_utc_timestamp(timestamp)
-                        if thread_ts is None:
-                            thread_ts = timestamp
-                        record['thread_ts'] = thread_ts
-                    else:
-                        record[date_field] = decimal_timestamp_to_utc_timestamp(timestamp)
+                    if stream == "messages":
+                        record["thread_ts"] = timestamp
+                    elif stream == "threads" and date_field == "ts":
+                        record["thread_ts"] = data[0].get("thread_ts", None)
+                    record[date_field] = decimal_timestamp_to_utc_timestamp(timestamp)
     return data
